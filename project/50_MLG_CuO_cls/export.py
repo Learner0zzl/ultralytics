9cb7fb8f281074bd3a1e54e60b60ca7cb38842de
learner_zzl <learner_zzl@163.com>
2026-05-29 15:14:54 +0800

    update 2026.05.29 周常更新：新增近期项目文件

---
+++ project/50_MLG_CuO_cls/export.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+dataset = "50_MLG_CuO_cls"
+name = "0520_e150_i320_b16"
+# Load the YOLO11 model
+model_path = rf"E:\Git\ultralytics\runs\classify\{dataset}\{name}\weights\best.pt"
+model = YOLO(model_path)
+model.export(format="onnx", batch=16)
+
+# 输入固定为images   输出固定为output  下面方法已通过推理验证
+import onnx
+onnx_model_path = model_path.replace('.pt', '.onnx')
+model = onnx.load(onnx_model_path)
+model.graph.input[0].name = "images"
+model.graph.output[0].name = "output"
+model.graph.node[-1].output[0] = "output"
+onnx.checker.check_model(model, full_check=True)
+onnx.save(model, onnx_model_path.replace('.onnx', '_renamed.onnx'))
---
+++ project/55_FY_Si_cls/train.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from ultralytics import YOLO
+
+
+def train():
+    # Load a model
+    model = YOLO("yolo11n-cls.yaml").load(r"E:\Git\ultralytics\weights\yolo11n-cls.pt")  # build from YAML and transfer weights
+    # model = YOLO(r"E:\Git\ultralytics\runs\classify\09_GN_coal_cls\1024_e150_i320_b16\weights\best.pt")  # build from YAML and transfer weights
+
+    # Train the model
+    subset = "0529_a1b15"
+    dataset = "55_FY_Si_cls"
+    name = "0529_e150_i320_b16"
+    results = model.train(data=rf"E:\Data\TrainSet\{dataset}\{subset}\images",
+                          cfg=rf"project\{dataset}\cfg.yaml",
+                          epochs=150, imgsz=320, batch=16, close_mosaic=15,
+                          name=rf"{dataset}\{name}")
+
+
+if __name__ == '__main__':
+    train()
